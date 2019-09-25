f42add60c4c1e8557e98684e8253ab824e40c3ce
Nicolas Damiani <nicolas.damiani@directa24.com>
2019-09-25 18:14:16 -0300

    initial promotions

--- CouponManager/app/controllers/promotions_controller.rb
+++ CouponManager/app/controllers/promotions_controller.rb
@@ -1,8 +1,22 @@
 class PromotionsController < ApplicationController
   def new
+    @promotion = Promotion.new
+  end
+
+  def show
+    @promotion = Promotion.find(promotion_id)
   end
 
   def create
+
+  end
+
+  def create
+    @promotion = Promotion.new(promotion_params)
+    @promotion.date = DateTime.current
+
+    if @promotion.save
+      redirect_to promotions_path
   end
 
   def update
@@ -12,5 +26,9 @@ class PromotionsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def index
+    @promotions = Promotion.all
   end
 end
