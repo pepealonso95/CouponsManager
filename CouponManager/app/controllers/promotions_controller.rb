c7ea48737b088466657be883bcc22a75d87f293c
Nicolas Damiani <nicolas.damiani@directa24.com>
2019-09-25 19:00:23 -0300

    basic crud operations promotion

--- CouponManager/app/controllers/promotions_controller.rb
+++ CouponManager/app/controllers/promotions_controller.rb
@@ -12,17 +12,25 @@ class PromotionsController < ApplicationController
   end
 
   def create
+    @organization = Organization.find(1);
     @promotion = Promotion.new(promotion_params)
-    @promotion.date = DateTime.current
 
     if @promotion.save
       redirect_to promotions_path
+    end
   end
 
   def update
+    @promotion = Promotion.find(promotion_id)
+    if @promotion.update(edit_promotion_params)
+      redirect_to promotions_path
+    else
+      render :edit
+    end
   end
 
   def edit
+    @promotion = Promotion.find(promotion_id)
   end
 
   def destroy
@@ -30,5 +38,17 @@ class PromotionsController < ApplicationController
 
   def index
     @promotions = Promotion.all
+  end
+
+  def promotion_params
+    params.require(:promotion).permit(:name, :cupon_code)
+  end
+
+  def edit_promotion_params
+    params.require(:promotion).permit(:name, :cupon_code)
+  end
+
+  def promotion_id
+    params.require(:id)
   end
 end
