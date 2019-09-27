90980f587db3795caced273d3f68d92afcc84542
Rafael Alonso <rafaeljosealonsogerla@gmail.com>
2019-09-27 19:01:01 -0300

    user register now working

--- CouponManager/app/controllers/application_controller.rb
+++ CouponManager/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    set_current_tenant_through_filter
+  
+    before_action :set_tenant
+  
+    def set_tenant
+      set_current_tenant(current_user.organization) if current_user.present?
+    end
 end
--- CouponManager/config/routes.rb
+++ CouponManager/config/routes.rb
@@ -1,6 +1,10 @@
 
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations' 
+  }
+  
   resources :users
   resources :organizations
   resources :promotions
