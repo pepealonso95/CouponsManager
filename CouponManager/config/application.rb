1908884cdde02607e22e9c7feb631b19177eea0d
Nicolas Damiani <nicolas.damiani@directa24.com>
2019-10-05 13:42:39 -0300

    logger configuration correctly

--- CouponManager/config/application.rb
+++ CouponManager/config/application.rb
@@ -12,6 +12,8 @@ module CouponManager
     config.load_defaults 6.0
     config.generators.jbuilder = false
 
+    # file = File.open('foo.log', File::WRONLY | File::APPEND | File::CREAT)
+    # logger = Logger.new('foo.log', 'daily')
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- CouponManager/config/environment.rb
+++ CouponManager/config/environment.rb
@@ -1,5 +1,13 @@
 # Load the Rails application.
 require_relative 'application'
 
+
+# file = File.open("log/#{Rails.env}-logs.log", File::WRONLY | File::APPEND )
+# logger = Logger.new('foo.log', 'daily')
+# Rails.logger = Logger.new(STDOUT, 'daily')
+# config.logger = ActiveSupport::Logger.new("log/#{Rails.env}-logs.log")
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
