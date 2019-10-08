e4bad33f72f29d4f797884bf288567e0397c9464
Rafael Alonso <rafaeljosealonsogerla@gmail.com>
2019-10-08 19:22:43 -0300

    transaction created

---
+++ CouponManager/db/migrate/20191008214243_create_transactions.rb
@@ -0,0 +1,9 @@
+class CreateTransactions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :transactions do |t|
+      t.belongs_to :promotion, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
