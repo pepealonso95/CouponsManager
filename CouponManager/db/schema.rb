a92838a93f7b01e888bba3e24d3a162126369ffb
Nicolas Damiani <nicolas.damiani@directa24.com>
2019-10-09 01:03:34 -0300

    bundle update

--- CouponManager/db/schema.rb
+++ CouponManager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_020148) do
+ActiveRecord::Schema.define(version: 2019_10_09_025847) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
